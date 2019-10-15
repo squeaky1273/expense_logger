1ee0fd5e660749df08e6cea4c33f6eabcf6d9567
squeaky1273 <pjriddell22@gmail.com>
2019-10-15 13:25:53 -0700

    added imports

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request, redirect url_for
+from pymongo import MongoClient
+from bson.ogjectid import ObjectId
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
