5c545059a4cae182bf050a20b0dd8e1f411c3b62
squeaky1273 <pjriddell22@gmail.com>
2019-10-15 14:46:28 -0700

    added home route

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 
+expense =db.expenses
+
 app = Flask(__name__)
 
 @app.route('/')
+def expense_index():
+    """Return homepage"""
+    return render_template('expense_index.html', expenses=expense.find())
