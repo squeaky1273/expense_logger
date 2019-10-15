b23096256c9439240e52e8ef83eb22465840a893
squeaky1273 <pjriddell22@gmail.com>
2019-10-15 15:01:06 -0700

    added submit route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect url_for
 from pymongo import MongoClient
-from bson.ogjectid import ObjectId
+from bson.objectid import ObjectId
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
@@ -17,6 +17,18 @@ def expense_index():
     return render_template('expense_index.html', expenses=expense.find())
 
 @app.route('/expense/new')
-    def expense_new():
-        """Return to the new expense log page"""
-        return render_template('expense_new.html', expense={}, title='New Expense Log')
+def expense_new():
+    """Return to the new expense log page"""
+    return render_template('expense_new.html', expense={}, title='New Expense Log')
+
+@app.route('/expense', methods=['POST'])
+def expense_submit():
+    """Submit a new expense log. User can add log information for purchases made"""
+    expense = {
+        'product name': request.form.get('product name'),
+        'price': request.form.get('price'),
+        'payment method': request.form.get('payment method')
+    }
+    print(expense)
+    expense_id = expense.insert_one(expense).inserted_id
+    return redirect(url_for('expense_show', expense_id=expense_id))
